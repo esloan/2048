db7c263e2fcd5900451a5d2ae5785b14bec023d2
esloan <esloan@imsa.edu>
2014-04-16 14:32:27 -0500

    now makes an (empty) window: I added a graphics package)

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,11 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class GUI {
+public class GUI extends GraphicsProgram {
 
+	
+	public static void main(String[] args){
+		String[] sizeArgs = { "width=" + 800, "height=" + 800 };
+
+	}
 }
